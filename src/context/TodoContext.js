0f2e2e1ad948725c5ed8224aae49e3f0f7ba4b48
xaanyaax <aanyasingh282004@gmail.com>
2025-06-24 15:05:12 +0530

    added context

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,10 +1,16 @@
 import { createContext , useContext } from "react";
 
 export const TodoContext = createContext({
-    todos : [
+    todos : [ //in the form of an array
         
         { id: 1, todo: "Learn React", completed: false },
-    ]
+        
+    ],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleTodo: (id) => {}
+
 })
 
 export const useTodo  = () => {
